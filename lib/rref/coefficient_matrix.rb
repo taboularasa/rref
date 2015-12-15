21f8b77c8d8149624dd431cc0f3f9cd90dcfb0d0
David Elliott <david@philosophie.is>
2015-12-15 06:29:28 -0800

    adds Rref::CoefficientMatrix#zero_out_remaining_column
    also adds supporting methods

--- lib/rref/coefficient_matrix.rb
+++ lib/rref/coefficient_matrix.rb
@@ -1,7 +1,7 @@
 module Rref
   class CoefficientMatrix
     Cursor = Struct.new(:x, :y)
-    attr_reader :rows
+    attr_reader :rows, :cursor
 
     def initialize(data)
       @cursor = Rref::CoefficientMatrix::Cursor.new(0,0)
@@ -12,6 +12,10 @@ module Rref
       Rref::Column.new rows.map { |r| r[index] }
     end
 
+    def row(index)
+      rows[index]
+    end
+
     def swap_rows(i, j)
       rows[i], rows[j] = rows[j], rows[i]
     end
@@ -20,18 +24,31 @@ module Rref
       rows[cursor.y][cursor.x]
     end
 
+    def advance_cursor
+      cursor.x += 1
+      cursor.y += 1
+    end
+
     def current_row
-      rows[cursor.y]
+      row(cursor.y)
+    end
+
+    def other_rows
+      rows.select {|r| r != current_row }
     end
 
     def current_column
       column(cursor.x)
     end
 
-    private
+    def reduce_current_row
+      current_row.reduce_row_by_leading_coefficient
+    end
 
-    def cursor
-      @cursor
+    def zero_out_remaining_column
+      other_rows.map do |row|
+        row.add! current_row.multiply(row[cursor.y] * -1)
+      end
     end
   end
 end
--- spec/rref/coefficient_matrix_spec.rb
+++ spec/rref/coefficient_matrix_spec.rb
@@ -16,6 +16,12 @@ describe Rref::CoefficientMatrix do
     end
   end
 
+  describe '#row' do
+    it 'is an alias for #rows subscript operator' do
+      expect(subject.row(0)).to eq(subject.rows[0])
+    end
+  end
+
   describe '#swap_rows' do
     it 'swaps two rows defined by their index' do
       original_row_positions = subject.rows.dup
@@ -27,16 +33,24 @@ describe Rref::CoefficientMatrix do
     end
   end
 
-  describe '#add_scaled_other_row' do
-    it 'adds another row that has been multiplied by a scalar'
-  end
-
   describe '#cursor_value' do
     it 'returns the value of the cell under the cursor' do
       cursor = double('Rref::Cursor', x: 0, y: 1)
       allow(subject).to receive(:cursor) { cursor }
 
       expect(subject.cursor_value).to eq(3)
+    end
+  end
+
+  describe '#advance_cursor' do
+    it 'increments the cursor.x and cursor.y by 1' do
+      starting_x = subject.cursor.x
+      starting_y = subject.cursor.y
+
+      subject.advance_cursor
+
+      expect(subject.cursor.x).to eq(starting_x + 1)
+      expect(subject.cursor.y).to eq(starting_y + 1)
     end
   end
 
@@ -49,12 +63,48 @@ describe Rref::CoefficientMatrix do
     end
   end
 
+  describe '#other_rows' do
+    it 'returns rows other than the current_row' do
+      expect(subject.other_rows.include? subject.current_row).to eq(false)
+    end
+  end
+
   describe '#current_column' do
     it 'returns the column currently under the cursor x axis' do
       cursor = double('Rref::Cursor', x: 1, y: 0)
       allow(subject).to receive(:cursor) { cursor }
 
       expect(subject.current_column).to eq(subject.column(1))
+    end
+  end
+
+  describe '#reduce_current_row' do
+    let(:row_1) { subject.row(0) }
+    let(:row_2) { subject.row(1) }
+
+    it 'sends reduce_row_by_leading_coefficient to the current row' do
+      expect(row_1)
+        .to receive(:reduce_row_by_leading_coefficient).and_call_original
+
+      subject.reduce_current_row
+
+      subject.advance_cursor
+
+      expect(row_2)
+        .to receive(:reduce_row_by_leading_coefficient).and_call_original
+
+      subject.reduce_current_row
+    end
+  end
+
+  describe '#zero_out_remaining_column' do
+    it 'add current_row to all other rows scaled by inverse of leading valule' do
+      subject = Rref::CoefficientMatrix.new([[1,4,6], [2,4,3], [4,3,1]])
+      subject.zero_out_remaining_column
+
+      expect(subject.current_column.fully_reduced?).to eq(true)
+      expect(subject.current_column.value_position)
+        .to eq(subject.cursor.y)
     end
   end
 end
